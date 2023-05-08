c8d5145c1e6f45b4c3c895b45fedf10f1991e979
Muskan <102637258+Fun-25@users.noreply.github.com>
2023-05-08 09:23:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,17 +6,16 @@ window.promises = [];
 
 for(let i=0;i<5;i++){
    let timeout=Math.floor(Math.random()*5)+1;
-   window.promise.push(new Promise((resolve,reject)=>{
+   window.promises.push(new Promise((resolve,reject)=>{
         setTimeout(()=>{
             resolve(`Promise ${i+1} resolved after ${timeout} seconds`)
         }, timeout*1000)
     }))
 }
-
-document.addEventListener("DOMContentLoaded", () => {
+ 
 	let output = document.getElementById("output")
 	  let p=document.createElement("p");
-  Promise.any(window.promise)
+  Promise.any(window.promises)
   .then(result => {
       
 	  p.innerHTML = result;
@@ -28,7 +27,33 @@ document.addEventListener("DOMContentLoaded", () => {
       // }
   })
   .catch(error => {
-          p.innerHTML = error;
-	    output.appendChild(p)
+          p.innerHTML = error;document.addEventListener("DOMContentLoaded", () => {
+  window.promises = [];
+
+  for(let i=0;i<5;i++){
+     let timeout=Math.floor(Math.random()*5)+1;
+     window.promises.push(new Promise((resolve,reject)=>{
+          setTimeout(()=>{
+              resolve(`Promise ${i+1} resolved after ${timeout} seconds`)
+          }, timeout*1000)
+      }))
+  }
+
+
+  let output = document.getElementById("output")
+
+  Promise.any(window.promises)
+  .then(result => {
+  
+    output.innerText = result;
+     
+  })
+  .catch(error => {
+     
+    output.innerText = error;
+    
   })
 })
+	    output.appendChild(p)
+  })
+
